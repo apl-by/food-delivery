1c08699e4ccf5312ac1ba02222a9c43c0f4a5be6
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-02-14 20:20:40 +0300

    add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
+import { Nunito } from "@next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,5 +17,14 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${nunito.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
