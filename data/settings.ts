276039b2d715782cbcb1450328becfeb93265e7d
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-03-09 15:01:16 +0300

    refactoring

--- data/settings.ts
+++ data/settings.ts
@@ -1,5 +1,2 @@
-// to split/join a displayName from Firebase User into firstName and secondName
-export const splitter = "&splitter&";
-
 export const asidePortalId = "id-aside-portal";
 export const modalPortalId = "id-modal-portal";
