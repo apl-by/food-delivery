0e3aff8cca579e55ab6a9b564bebfa6d9f37f740
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-03-10 23:29:20 +0300

    add recaptchacheck

--- utils/init-firebase.ts
+++ utils/init-firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
-// import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,13 +14,15 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
-// const appCheck = initializeAppCheck(app, {
-//   provider: new ReCaptchaV3Provider("6Ldur-ckAAAAAK5kIDv3x2GKabJJXXAW_at9oZtl"),
-//   // Optional argument. If true, the SDK automatically refreshes App Check
-//   // tokens as needed.
-//   isTokenAutoRefreshEnabled: true,
-// });
-
 export const auth = getAuth(app);
+
+if (typeof document !== "undefined") {
+  const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(
+      "6Ldur-ckAAAAAK5kIDv3x2GKabJJXXAW_at9oZtl"
+    ),
+    isTokenAutoRefreshEnabled: true,
+  });
+}
+
 export const database = getDatabase(app);
